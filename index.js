99eaa273a3ef7f780cfec96cfae633585e0a2ff5
nrsabit <mailbox.sabit@gmail.com>
2023-06-01 17:38:52 +0600

    user update and deletion added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -58,6 +59,13 @@ async function run() {
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id
+      const query = {_id : new ObjectId(id)}
+      const result = await usersCollection.deleteOne(query)
+      res.send(result)
+    })
 
     // menu related apis
     app.get("/menus", async (req, res) => {
